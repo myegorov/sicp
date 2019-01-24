78ed897348d981bdb858cc383d06357e45e0df18
Maksim Yegorov <findmaksim@gmail.com>
2019-01-23 22:54:34 -0800

    todo: ex. 1.19 onwards

--- exercises/1/1.2.4.rkt
+++ exercises/1/1.2.4.rkt
@@ -1,54 +1,62 @@
 #lang sicp
 (#%require racket/trace)
 
-;; Ex. 1.16
-;; recursive exponentiation
+;; translate definition of exponentiation
+;; linear recursive version
 (define (pow-rec b n)
+  (if (= n 0)
+      1
+      (* b (pow-rec b (- n 1)))))
+
+;; O(n) time, O(1) space iterative version
+(define (pow-iter b n)
+  (define (helper b counter product)
+    (if (= counter 0)
+	product
+	(helper b (dec counter) (* b product))))
+  (helper b n 1))
+
+;; O(log n) time, O(log n) space version
+(define (square x) (* x x))
+(define (pow-log b n)
   (cond ((= n 0) 1)
-	((= 0 (remainder n 2)) (square (pow-rec b (/ n 2))))
-	(else (* b (pow-rec b (- n 1))))))
+	((= (remainder n 2) 0) (square (pow-log b (/ n 2))))
+	(else (* b (pow-log b (- n 1))))))
 
-(define (square x)
-  (* x x))
+;; Ex. 1.16
+;; O(log n) time, O(1) space version
+(define (pow-log-iter b n)
+  (define (helper b counter product)
+    (cond ((= counter 0) product)
+	  (else (helper b
+			(- counter 1)
+			(* product (square b))))))
+  (if (= (remainder n 2) 0)
+      (helper b (/ n 2) 1)
+      (helper b (/ (- n 1) 2) b)))
 
-;; iterative eponentiation
-(define (pow b n)
-  (define (pow-iter b-squared n tot)
-    (cond ((= n 0) tot)
-	  (else (pow-iter b-squared (- n 1) (* tot b-squared)))))
-  
-  (cond ((= 0 (remainder n 2)) (pow-iter (square b) (/ n 2) 1))
-	(else (pow-iter (square b) (/ (- n 1) 2) b))))
-
-
-;; Ex 1.17
-(define (double x)
-  (+ x x))
-(define (halve x)
-  ;; precondition: (even? x) => #t
-  (/ x 2))
-(define (even? x)
-  (= (remainder x 2) 0))
-
-;; not yet tail recursive
-(define (* x y)
-  ;; precondition: (or (< x 0) (< y 0)) => #f
-  (define (** b n tot)
-    (cond ((= n 0) 0)
-	  ((= n 1) tot)
-	  ((even? n) (** b (halve n) (double tot)))
-	  (else (+ b (** b (halve (- n 1)) (double tot))))))
-
-  (** x y x))
+;; Ex. 1.17
+;; define recursive multiplication
+;; O(log n) in both space and time
+(#%require scheme/fixnum)
+;; use bitwise shift for double/halve
+(define (double x) (fxlshift x 1))
+(define (halve x) (fxrshift x 1))
+(define (mult-rec x y)
+  (cond ((or (= x 0) (= y 0)) 0)
+	((= y 1) x)
+	((= (remainder y 2) 0)
+	 (mult-rec (double x) (halve y)))
+	(else (+ x (mult-rec x (- y 1))))))
 
 ;; Ex. 1.18
-;; iterative implementation of Ex. 1.17
+;; define iterative multiplication
+;; O(log n) time, O(1) space
 (define (mult-iter x y)
-  ;; precondition: (or (< x 0) (< y 0)) => #f
-  (define (** x y init)
-    (cond ((or (= y 0) (= x 0)) 0)
-	  ((= y 1) (+ x init))
-	  ((even? y) (** (double x) (halve y) init))
-	  (else (** (double x) (halve (- y 1)) x))))
- 
-  (** x y 0))
+  (define (helper x y acc)
+    (cond ((or (= x 0) (= y 0)) x)
+	  ((= y 1) (+ x acc))
+	  ((= (remainder y 2) 0)
+	   (helper (double x) (halve y) acc))
+	  (else (helper x (- y 1) (+ x acc)))))
+  (helper x y 0))
